3c67347eef457e6f82191cae096555914cbb8cfa
alaredosr <alaredosr@hotmail.com>
2021-05-22 14:59:59 -0600

    My Hello line was added.

--- src/tsosie/Main.java
+++ src/tsosie/Main.java
@@ -3,6 +3,8 @@ package tsosie;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Everett Tsosie.");
+
+        System.out.println("Hello, my name is Everett Tsosie.");
+        System.out.println("Hello, Everett Tsosie, my name is Armando.");
     }
 }
